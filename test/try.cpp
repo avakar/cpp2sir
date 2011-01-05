2c7af2e73ac1f9a4fef2ed9929c6d0ecba1b205f
Martin Vejnar <avakar@ratatanek.cz>
2011-01-05 16:56:35 +0100

    Added some more cases to try test.

--- test/try.cpp
+++ test/try.cpp
@@ -1,4 +1,40 @@
-int f1()
+struct s { s(); ~s(); };
+
+void e1();
+void e2(int);
+void e3();
+
+void f1()
+{
+    throw 1;
+}
+
+void f2()
+{
+    throw s();
+}
+
+void f3()
+{
+    try
+    {
+        e1();
+    }
+    /*catch (int & e)
+    {
+        e2(e);
+    }*/
+    catch (long)
+    {
+        e3();
+    }
+    catch (...)
+    {
+        e3();
+    }
+}
+
+int f4()
 {
     try
     {
