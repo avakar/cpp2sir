3ceef2fcf54406b9ec8ab7554cd825cfc3c79333
Martin Vejnar <avakar@ratatanek.cz>
2011-01-16 23:51:47 +0100

    pretty_print.py can now emit real node numbers (-r argument).

--- tools/pretty_print.py
+++ tools/pretty_print.py
@@ -18,64 +18,70 @@ def _pretty_print_node(node, nodemap):
 
     return inst
 
-import sys, json
+if __name__ == '__main__':
+    import sys, json
+    real_nodenums = '-r' in sys.argv[1:] or '--real' in sys.argv[1:]
 
-j = json.load(sys.stdin)
+    j = json.load(sys.stdin)
 
-for fnname, fn in j['cfgs'].iteritems():
-    print 'def %s(%s):' % (fnname, ', '.join(fn['params']))
+    for fnname, fn in j['cfgs'].iteritems():
+        print 'def %s(%s):' % (fnname, ', '.join(fn['params']))
 
-    entry = fn['entry']
-    nodes = fn['nodes']
-    nodemap = {}
-    entry_stack = range(len(nodes))
-    entry_stack.append(entry)
-    i = 0
+        entry = fn['entry']
+        nodes = fn['nodes']
+        nodemap = {}
+        entry_stack = range(len(nodes))
+        entry_stack.append(entry)
+        i = 0
 
-    while entry_stack:
-        entry = entry_stack.pop()
+        while entry_stack:
+            entry = entry_stack.pop()
 
-        nodeq = [entry]
-        while nodeq:
-            n = nodeq.pop(0)
-            if n in nodemap:
-                continue
+            nodeq = [entry]
+            while nodeq:
+                n = nodeq.pop(0)
+                if n in nodemap:
+                    continue
 
-            while n is not None:
-                nodemap[n] = i
-                i += 1
+                while n is not None:
+                    nodemap[n] = i
+                    i += 1
 
-                type, succs, ops = nodes[n][:3]
-                n = None
-                for target, index, cond in succs:
-                    if index == 0 and cond is None and target not in nodemap:
-                        n = target
-                    elif target not in nodemap:
-                        nodeq.append(target)
+                    type, succs, ops = nodes[n][:3]
+                    n = None
+                    for target, index, cond in succs:
+                        if index == 0 and cond is None and target not in nodemap:
+                            n = target
+                        elif target not in nodemap:
+                            nodeq.append(target)
 
-    inverse_nodelist = range(len(nodemap))
-    for key, value in nodemap.iteritems():
-        inverse_nodelist[value] = key
+        inverse_nodelist = range(len(nodemap))
+        for key, value in nodemap.iteritems():
+            inverse_nodelist[value] = key
 
-    for i, n in enumerate(inverse_nodelist):
-        node = nodes[n]
+        for i, n in enumerate(inverse_nodelist):
+            node = nodes[n]
 
-        succs = sorted([(index, cond, nodemap[target]) for target, index, cond in node[1]])
-        strsuccs = []
-        separate_next = True
-        for index, cond, target in succs:
-            condstr = repr(cond) + ' ' if cond is not None else ''
-        
-            if index == 0 and cond is None and target == i + 1:
-                separate_next = False
-            elif index == 0:
-                strsuccs.append('%s-> $%d' % (condstr, target))
-            else:
-                strsuccs.append('%s->%d $%d' % (condstr, index, target))
+            succs = sorted([(index, cond, nodemap[target]) for target, index, cond in node[1]])
+            strsuccs = []
+            separate_next = True
+            for index, cond, target in succs:
+                condstr = repr(cond) + ' ' if cond is not None else ''
+            
+                if index == 0 and cond is None and target == i + 1:
+                    separate_next = False
+                else:
+                    if real_nodenums:
+                        target = inverse_nodelist[target]
+                    if index == 0:
+                        strsuccs.append('%s-> $%d' % (condstr, target))
+                    else:
+                        strsuccs.append('%s->%d $%d' % (condstr, index, target))
 
-        res = '    $%d: %s' % (i, _pretty_print_node(node, nodemap))
-        if strsuccs:
-            res += ' | ' + ', '.join(strsuccs)
-        if separate_next:
-            res += '\n'
-        print res
+            res = '    $%d: %s' % (n if real_nodenums else i,
+                _pretty_print_node(node, nodemap if not real_nodenums else { i : i for i in xrange(len(nodes))}))
+            if strsuccs:
+                res += ' | ' + ', '.join(strsuccs)
+            if separate_next:
+                res += '\n'
+            print res
