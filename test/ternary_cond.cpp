a7882a248acc68f450cd7bfd17ca47d4ff45e345
Martin Vejnar <avakar@ratatanek.cz>
2011-01-17 12:36:22 +0100

    Fixed some tests.

--- test/ternary_cond.cpp
+++ test/ternary_cond.cpp
@@ -3,11 +3,6 @@ int f1(int i)
     return i > 89? 42: 43;
 }
 
-int f2(int i)
-{
-    throw 4;
-}
-
 int f3(int i)
 {
     return i? i: throw 4;
@@ -18,7 +13,7 @@ void f4(int i)
     i? throw 3: throw 4;
 }
 
-int e1(int const & a, int const & b);
+int e1(int const & a, int const & b) throw();
 
 int const & f5(int const & lhs, int const & rhs)
 {
